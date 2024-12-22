da814bd3ebda4382bc7431ce3c25ab2620089adc
Octaplus <ebizhonix@gmail.com>
2024-12-22 12:55:29 +0100

    javascript string property and methods

---
+++ javaScriptBasics.js
@@ -0,0 +1,24 @@
+// this is a comment
+// this is a string data type
+"Emmanuel", "Watson-Aputu";
+typeof "Emmanuel";
+typeof "Watson-Aputu";
+typeof 21;
+typeof true;
+
+let myName = "Emmanuel";
+// there are four data types they are;
+let stringDataType = "string";
+
+let numberDataType = 21;
+
+let booleanDataType = true;
+
+let undefinedVariable;
+
+function addNumber(num1, num2) {
+  return num1 + num2;
+}
+const result = addNumber(4, 8);
+
+consoleninja.log(result);
---
+++ js/main.js
@@ -0,0 +1,22 @@
+const myVariable = "Mathematics";
+
+// the length property
+/**returns the length if the string variable */
+
+console.log(myVariable.length); // 11
+
+// the string methods
+/* 1. CHARACTER AT USING .charAt(): returns the value
+at the provided index*/
+
+console.log(myVariable.charAt(6)); //
+
+// 2. INDEX OF()
+/*returns the character at the provided index. if more 
+than one character is provided it returns the first 
+instance of the occurence. */
+console.log(myVariable.indexOf("at")); // 1
+
+// 3. LAST INDEX OF()
+// the exact opposite of indexOf.
+console.log(myVariable.lastIndexOf("at")); // 10
